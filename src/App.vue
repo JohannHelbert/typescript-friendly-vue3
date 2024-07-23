3b03dc7c0e6e928ec7415885d11ada8f485356b5
passionate skye <johannhelbert@gmail.comgit config --global user.name JohannHelbert>
2024-07-23 14:44:43 +0200

    add callback functions

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted, computed } from 'vue'
+import fetchCount from './services/fetchCount'
+
+interface AppInfo {
+  name: string,
+  slogan: string
+}
+
+const count = ref<number | null>(null)
+
+const appInfo: AppInfo = reactive({
+  name: 'Counter',
+  slogan: 'an app you can count on'
+})
+
+onMounted(() => {
+	fetchCount((initialCount) => {
+		count.value = initialCount
+	})
+})
+
+function addCount(num: number) {
+  if (count.value !== null) {
+    count.value += num
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ appInfo.name }}</h1>
+    <h2>{{ appInfo.slogan }}</h2>
+  </div>
+	<p>{{ count }}</p>
+  <p>
+    <button @click='addCount(1)'>Add</button>
+  </p>
+</template>
